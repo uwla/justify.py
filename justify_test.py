72db1c7745e892d13038fbb96caefbc97f36d42b
uwla <uwlalinux@gmail.com>
2023-09-06 16:36:38 -0300

    add more tests

--- justify_test.py
+++ justify_test.py
@@ -45,3 +45,72 @@ def test_is_start_of_list_item():
         assert justify.is_start_of_list_item(test)
     for test in tests_2:
         assert not justify.is_start_of_list_item(test)
+
+def test_is_latex_command():
+    tests_1 = [
+        '\\section{Chapter 1}',
+        '\\begin{center}',
+        '\\includegraphics{image.png}',
+        '\\includegraphics[width=1]{image.png}',
+        '\\end{center}',
+    ]
+    tests_2 = [
+        'begin{center}',
+        '\\textbf{Python}: a programming language',
+        'end{center}',
+    ]
+    for test in tests_1:
+        assert justify.is_latex_command(test)
+    for test in tests_2:
+        assert not justify.is_latex_command(test)
+
+def test_text2blocks():
+    tests = [
+        # test 1
+        """
+A quo autem hic quis id neque. Dolor numquam non iure.
+Quod ipsum officia ad repudiandae est id.
+
+Eveniet dolores quis debitis. Deserunt quod tempora rerum ea hic cum.
+Est aperiam velit corrupti.
+Laudantium et laboriosam placeat quia consequatur perspiciatis id molestias.
+
+Blanditiis magnam consequatur aperiam rerum rerum.
+Voluptas cumque rerum et molestias quos at quis.""",
+        # test 2
+        """A quo autem hic quis id neque. Dolor numquam non iure.
+
+
+
+Blanditiis magnam consequatur aperiam rerum rerum.
+Voluptas cumque rerum et molestias quos at quis.
+""",
+
+    ]
+    expected = [
+        # test 1
+        [
+            '',
+            """A quo autem hic quis id neque. Dolor numquam non iure.
+Quod ipsum officia ad repudiandae est id.""",
+            '',
+            """Eveniet dolores quis debitis. Deserunt quod tempora rerum ea hic cum.
+Est aperiam velit corrupti.
+Laudantium et laboriosam placeat quia consequatur perspiciatis id molestias.""",
+            '',
+            """Blanditiis magnam consequatur aperiam rerum rerum.
+Voluptas cumque rerum et molestias quos at quis.""",
+        ],
+        # test 2
+        [
+            "A quo autem hic quis id neque. Dolor numquam non iure.",
+            '', '', '',
+            """Blanditiis magnam consequatur aperiam rerum rerum.
+Voluptas cumque rerum et molestias quos at quis.""",
+        ],
+    ]
+
+    for i in range(0, len(tests)):
+        blocks = justify.text2blocks(tests[i])
+        assert len(blocks) == len(expected[i])
+        assert blocks == expected[i]
